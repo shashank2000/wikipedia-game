cabafa1fdbb71d4030e2875ff0cc85e812298935
Shashank Rammoorthy <sr22@stanford.edu>
2019-10-29 15:31:36 -0700

    fixed list repetition issues

--- app.py
+++ app.py
@@ -24,7 +24,4 @@ def form_post():
     return template('result.tpl', result)
 
 
-if os.environ.get('APP_LOCATION') == 'heroku':
-    run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
-else:
-    run(host='localhost', port=8080, debug=True)
+run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
